e2ced9729d6b122a1e978301aafb9380a05c53f1
david.lin <linyongxi7@gmail.com>
2018-06-09 11:17:20 +1000

    remove useless file

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -8,3 +8,13 @@ source ~/.dotfiles/zsh/functions/functions.zsh
 source ~/.secrets
 
 setopt AUTO_CD
+
+# export vim
+export EDITOR="vim"
+bindkey -v
+
+# vi style incremental search
+bindkey '^R' history-incremental-search-backward
+bindkey '^S' history-incremental-search-forward
+bindkey '^P' history-search-backward
+bindkey '^N' history-search-forward
